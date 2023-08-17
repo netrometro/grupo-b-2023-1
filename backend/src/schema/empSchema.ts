d4eb51a5db8f365795f7871bcbbceb26ee29d11c
Nome <miranhaomi@gmail.com>
2023-08-17 20:14:21 -0300

    Schemas Atualizados
    Foi feito a atualização dos schemas de empresa e ficha de funcionário, colocando nos arquivos o que é preciso para funcionamento da validação dos dados.

---
+++ backend/src/schema/empSchema.ts
@@ -0,0 +1,11 @@
+import { z } from "zod";
+
+export const EmpSchema = z.object({
+  nome: z.string(),
+  cnpj: z.string(),
+  endereco: z.string(),
+  cep: z.string(),
+  administradorId: z.number(),
+});
+
+export type EmpData = z.infer<typeof EmpSchema>;
---
+++ backend/src/schema/fichaSchema.ts
@@ -0,0 +1,19 @@
+import { z } from "zod";
+
+export const FichaFuncionarioSchema = z.object({
+  nome: z.string(),
+  email: z.string().email(),
+  nascimento: z.string().transform((val) => new Date(val)),
+  nacionalidade: z.string(),
+  cpf: z.string(),
+  rg: z.string(),
+  cargo: z.string(),
+  endereco: z.string(),
+  pispasep: z.string(),
+  admissao: z.string().transform((val) => new Date(val)),
+  formacao: z.string(),
+  ctps: z.string(),
+  empresaId: z.number(),
+});
+
+export type FichaFuncionarioData = z.infer<typeof FichaFuncionarioSchema>;
