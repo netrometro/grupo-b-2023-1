5904562d3048375dc3f2fe9c1ccfa109267d6c13
Lucas Monteiro <77646992+lucas752@users.noreply.github.com>
2023-08-17 21:38:54 -0300

    feat: criando base do componente Input

---
+++ frontend/src/components/Input/index.tsx
@@ -0,0 +1,6 @@
+import { View } from 'react-native';
+import stylesInput from './styles';
+
+export default function Input() {
+  return <View style={stylesInput.container}></View>;
+}
---
+++ frontend/src/components/Input/styles.ts
@@ -0,0 +1,7 @@
+import { StyleSheet } from 'react-native';
+
+const stylesInput = StyleSheet.create({
+  container: {},
+});
+
+export default stylesInput;
