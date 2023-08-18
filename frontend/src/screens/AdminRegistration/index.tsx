0d7d5d612057ab473d2eedc8be82da4cea8fa6ab
Lucas Monteiro <77646992+lucas752@users.noreply.github.com>
2023-08-18 19:35:39 -0300

    feat: criando a função de cada campo da tela de cadastrar administrador

--- frontend/src/screens/AdminRegistration/index.tsx
+++ frontend/src/screens/AdminRegistration/index.tsx
@@ -3,19 +3,57 @@ import stylesAdminRegistration from './styles';
 import Navbar from '../../components/Navbar';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
+import { useState } from 'react';
 
 export default function AdminRegistration() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState('');
+  const [cpf, setCpf] = useState('');
+
+  const data = {
+    email: email,
+    password: password,
+    username: username,
+    cpf: cpf,
+  };
+
   return (
     <View style={stylesAdminRegistration.container}>
       <Navbar onPressArrowLeft={() => {}} text="Cadastre-se" />
       <View style={stylesAdminRegistration.body}>
         <View style={stylesAdminRegistration.inputs}>
-          <Input error={false} label="E-mail:" placeholder="email@email.com" onChange={() => {}} />
-          <Input error={false} label="Senha:" placeholder="********" onChange={() => {}} />
-          <Input error={false} label="Nome:" placeholder="Nome Completo" onChange={() => {}} />
-          <Input error={false} label="CPF:" placeholder="000.000.000-00" onChange={() => {}} />
+          <Input
+            error={false}
+            label="E-mail:"
+            placeholder="email@email.com"
+            onChange={(value: string) => setEmail(value)}
+          />
+          <Input
+            error={false}
+            label="Senha:"
+            placeholder="********"
+            onChange={(value: string) => setPassword(value)}
+          />
+          <Input
+            error={false}
+            label="Nome:"
+            placeholder="Nome Completo"
+            onChange={(value: string) => setUsername(value)}
+          />
+          <Input
+            error={false}
+            label="CPF:"
+            placeholder="000.000.000-00"
+            onChange={(value: string) => setCpf(value)}
+          />
         </View>
-        <Button text="CADASTRAR" onPress={() => {}} />
+        <Button
+          text="CADASTRAR"
+          onPress={() => {
+            console.log(data);
+          }}
+        />
       </View>
     </View>
   );
