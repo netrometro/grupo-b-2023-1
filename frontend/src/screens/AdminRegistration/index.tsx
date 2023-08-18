71b108a82120e1d3ea57c8ad35e5119bff9e88a1
Lucas Monteiro <77646992+lucas752@users.noreply.github.com>
2023-08-18 15:22:39 -0300

    feat: adicionando base da tela de cadastro de administrador

---
+++ frontend/src/screens/AdminRegistration/index.tsx
@@ -0,0 +1,6 @@
+import { View } from 'react-native';
+import stylesAdminRegistration from './styles';
+
+export default function AdminRegistration() {
+  return <View style={stylesAdminRegistration.container}></View>;
+}
---
+++ frontend/src/screens/AdminRegistration/styles.ts
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+
+const stylesAdminRegistration = StyleSheet.create({
+  container: {
+    width: '100%',
+    alignItems: 'center',
+  },
+});
+
+export default stylesAdminRegistration;
