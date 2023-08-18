3a425534095080aa4c7461abf0f4f73eba2ef053
Lucas Monteiro <77646992+lucas752@users.noreply.github.com>
2023-08-18 15:14:22 -0300

    feat: adicionando função para o 'minhas informações'

--- frontend/src/screens/Dashboard/index.tsx
+++ frontend/src/screens/Dashboard/index.tsx
@@ -4,11 +4,15 @@ import DashboardNavbar from './components/DashboarNavbar';
 import Button from '../../components/Button';
 
 export default function Dashboard() {
+  const handleMyInformations = () => {
+    console.log('cliquei no minhas informações');
+  };
+
   return (
     <View style={stylesDashboard.container}>
       <DashboardNavbar />
       <View style={stylesDashboard.body}>
-        <Button onPress={() => {}} text="Minhas informações" />
+        <Button onPress={handleMyInformations} text="Minhas informações" />
       </View>
     </View>
   );
