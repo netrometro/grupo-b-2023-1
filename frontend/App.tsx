f865c56ae28c42ff2df6a59437e87bb50b17d9c3
Lucas Monteiro <77646992+lucas752@users.noreply.github.com>
2023-08-17 21:03:01 -0300

    feat: adicionando componente de login no App.tsx e trocando cor de fundo da aplicação

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Login from './src/screens/Login';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Login />
     </View>
   );
 }
@@ -13,8 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#d9d9d9',
     alignItems: 'center',
-    justifyContent: 'center',
   },
 });
