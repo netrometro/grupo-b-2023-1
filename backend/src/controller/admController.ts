e49eaed4bb93f26a31abbbdf2444637503cacbfe
Lucas Monteiro <77646992+lucas752@users.noreply.github.com>
2023-08-18 22:38:17 -0300

    feat: encriptografando a senha

--- backend/src/controller/admController.ts
+++ backend/src/controller/admController.ts
@@ -1,5 +1,6 @@
 import { FastifyRequest, FastifyReply } from "fastify";
 import admSchema from "../schema/admSchema";
+import bcrypt from "bcrypt";
 
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
@@ -7,17 +8,19 @@ const prisma = new PrismaClient();
 exports.createAdm = async (req: FastifyRequest, res: FastifyReply) => {
   const { nome, email, senha, cpf } = admSchema.parse(req.body);
 
+  const hashedSenha = bcrypt.hashSync(senha, 10);
+
   const administrador = await prisma.administrador.create({
     data: {
       nome,
       email,
-      senha,
+      senha: hashedSenha,
       cpf,
     },
     select: {
       id: true,
       email: false,
-      senha: false,
+      senha: true,
       cpf: false,
     },
   });
