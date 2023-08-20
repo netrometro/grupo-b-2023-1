de272667f1b8ec1b5bcd577695248ce5e8f00c97
Nome <miranhaomi@gmail.com>
2023-08-20 13:23:58 -0300

    Criando Interfaces Restantes
    Foi criado as interfaces Employer.ts e a Emp.ts, responsáveis por terem a interface do objeto da ficha de funcionário e da empresa.

---
+++ frontend/src/interfaces/employer.ts
@@ -0,0 +1,16 @@
+export interface Employer {
+    id: number;
+    nome: string;
+    email: string;
+    nascimento: Date;
+    nacionalidade: string;
+    cpf: string;
+    rg: string;
+    cargo: string;
+    endereco: string;
+    pispasep: string;
+    admissao: Date;
+    formacao: string;
+    ctps: string;   
+}
+  
