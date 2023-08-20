a5ec92776db5aa19cd05bb81c68e1da3e7bd426b
Nome <miranhaomi@gmail.com>
2023-08-20 13:30:25 -0300

    Atualizando o Dashboard
    Atualizando o componente do Dashboard para agora ele receber o handleCreateCompany que quando o usuário clicar em adicionar empresa, ele vai para a página de criação. Além disso, agora ele tem o ListCompany que vai listar as empresas.

--- frontend/src/screens/Dashboard/index.tsx
+++ frontend/src/screens/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import stylesDashboard from './styles';
 import DashboardNavbar from './components/DashboarNavbar';
 import Button from '../../components/Button';
@@ -7,6 +7,8 @@ import { useEffect, useState } from 'react';
 import { api } from '../../services/api';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { User } from '../../interfaces/user';
+import React from 'react';
+import ListCompany from '../ListCompany';
 
 export default function Dashboard() {
   const [user, setUser] = useState<User>();
@@ -34,11 +36,17 @@ export default function Dashboard() {
     navigate('adminEditInfo');
   };
 
+  const handleCreateCompany = () => {
+    navigate('companyRegistration')
+  }
+
   return (
     <View style={stylesDashboard.container}>
       <DashboardNavbar />
       <View style={stylesDashboard.body}>
         <Button onPress={handleMyInformations} text="Minhas informações" />
+        <Button onPress={handleCreateCompany} text="Adicionar Empresa" />
+        <ListCompany />
       </View>
     </View>
   );
