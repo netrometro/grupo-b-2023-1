ceee3f5f9a2daf4edefafa732aece0e151308a00
Lucas Monteiro <77646992+lucas752@users.noreply.github.com>
2023-08-21 12:16:18 -0300

    fix: adicionando verificações nas requisições do administrador

--- backend/src/schema/admSchema.ts
+++ backend/src/schema/admSchema.ts
@@ -2,14 +2,25 @@ import { z } from "zod";
 
 export const admSchema = z.object({
   nome: z.string(),
-  email: z.string().email(),
-  senha: z.string(),
+  email: z.string().email({
+    message: "E-mail inválido",
+  }),
+  senha: z.string().min(8, {
+    message: "A senha precisa ter pelo menos 8 dígitos",
+  }),
   cpf: z.string(),
 });
 
 export const admSchemaEdit = z.object({
   nome: z.string(),
-  email: z.string().email(),
-  senha: z.string().nullable(),
+  email: z.string().email({
+    message: "E-mail inválido",
+  }),
+  senha: z
+    .string()
+    .min(8, {
+      message: "A senha precisa ter pelo menos 8 dígitos",
+    })
+    .nullable(),
   cpf: z.string(),
 });
