adb4813aefcc064c33e9384ba16e60987b2d3515
elise <Elisenovaes10@gmail.com>
2023-08-22 17:18:51 -0300

    Update index.tsx

--- frontend/src/screens/CompanyDashboard/index.tsx
+++ frontend/src/screens/CompanyDashboard/index.tsx
@@ -7,6 +7,7 @@ import styles from './style';
 import stylesDashboardNavbar from '../../components/Navbar/styles'; // Importar o estilo do Navbar
 import { useNavigation } from '@react-navigation/native';
 import EmployeeList from '../EmployeeList';
+import Button from '../../components/Button';
 
 interface CompanyDashboardProps {
   route: { params: { companyId: number } };
@@ -43,6 +44,10 @@ const CompanyDashboard: React.FC<CompanyDashboardProps> = ({ route }) => {
 
   const { navigate, goBack } = useNavigation();
 
+  const handleRegistrationEmployee = () => {
+    navigate('fichaRegistration');
+  }
+
   return (
     <View style={styles.container}>
       <View style={stylesDashboardNavbar.container}>
@@ -64,6 +69,7 @@ const CompanyDashboard: React.FC<CompanyDashboardProps> = ({ route }) => {
       onPress={() => navigate('dashboard')}
       >
         <Text style={styles.backButtonText}>Voltar</Text>
+        <Button onPress={handleRegistrationEmployee} text='Adicionar funcionário'/>
       </TouchableOpacity>
       <EmployeeList companyId={companyId}/>
     </View>
