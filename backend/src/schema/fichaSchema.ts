bc7b3113dcedf30451f9adf785aa955a3e946c6d
Lucas Monteiro <77646992+lucas752@users.noreply.github.com>
2023-08-22 20:21:38 -0300

    fix: alterando tipo da data

--- backend/src/schema/fichaSchema.ts
+++ backend/src/schema/fichaSchema.ts
@@ -3,17 +3,16 @@ import { z } from "zod";
 export const FichaFuncionarioSchema = z.object({
   nome: z.string(),
   email: z.string().email(),
-  nascimento: z.string(),
+  nascimento: z.string().datetime(),
   nacionalidade: z.string(),
   cpf: z.string(),
   rg: z.string(),
   cargo: z.string(),
   endereco: z.string(),
   pispasep: z.string(),
-  admissao: z.string(),
+  admissao: z.string().datetime(),
   formacao: z.string(),
   ctps: z.string(),
 });
 
 export type FichaFuncionarioData = z.infer<typeof FichaFuncionarioSchema>;
-
