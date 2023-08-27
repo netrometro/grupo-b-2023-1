917949b3ee262908b76583033b061b037b4d6324
Nome <miranhaomi@gmail.com>
2023-08-27 18:27:11 -0300

    Update index.tsx

--- frontend/src/screens/DemisionList/index.tsx
+++ frontend/src/screens/DemisionList/index.tsx
@@ -1,44 +1,31 @@
 import React, { useEffect, useState } from 'react';
-import {
-  View,
-  Text,
-  FlatList,
-  TouchableOpacity,
-  ToastAndroid,
-  ActivityIndicator,
-} from 'react-native';
+import { View, Text, FlatList, TouchableOpacity, ActivityIndicator } from 'react-native';
 import { api } from '../../services/api';
 import { Employer } from '../../interfaces/employer';
 import stylesEmployeeList from '../EmployeeList/style';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { useNavigation } from '@react-navigation/native';
-import { DotsThree, Trash, UserSquare } from 'phosphor-react-native';
-
-type Nav = {
-  navigate: (value: string, ids?: object) => void;
-};
 
 interface Props {
   companyId: number;
 }
 
 export default function DemisionList({ companyId }: Props) {
-  const [employees, setEmployees] = useState<Employer[]>([]);
-  const [isLoading, setisLoading] = useState(false);
+  const [demitidos, setDemitidos] = useState<Employer[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    const fetchEmployees = async () => {
+    const fetchDemitidos = async () => {
       try {
-        setisLoading(true);
+        setIsLoading(true);
         const adminId = await AsyncStorage.getItem('adminId');
 
         if (!adminId) {
           console.error('ID de administrador inválido');
-          setisLoading(false);
+          setIsLoading(false);
           return;
         }
 
-         console.log("companyId: ", companyId);
+        console.log('companyId: ', companyId);
 
         const response = await api.get(`/demitidos/${companyId}`, {
           headers: {
@@ -46,18 +33,16 @@ export default function DemisionList({ companyId }: Props) {
           },
         });
 
-        setEmployees(response.data);
-        setisLoading(false);
+        setDemitidos(response.data);
+        setIsLoading(false);
       } catch (error) {
         console.error(error);
-        setisLoading(false);
+        setIsLoading(false);
       }
     };
 
-    fetchEmployees();
+    fetchDemitidos();
   }, []);
-
-  const { navigate } = useNavigation<Nav>();
 
   return (
     <View style={stylesEmployeeList.container}>
@@ -65,27 +50,14 @@ export default function DemisionList({ companyId }: Props) {
         <ActivityIndicator size="large" color="#4F67D8" />
       ) : (
         <FlatList
-          data={employees}
+          data={demitidos}
           style={stylesEmployeeList.flatList}
           keyExtractor={(item) => item.nome}
           renderItem={({ item }) => (
-            <View
-              style={stylesEmployeeList.employeeItem}
-              // onPress={() => navigate('editEmployer', { companyId, employeeId: item.id })}
-            >
+            <View style={stylesEmployeeList.employeeItem}>
               <View style={stylesEmployeeList.iconNameContainer}>
-                <UserSquare size={52} weight="fill" color="#4F67D8" />
-                <View>
-                  <Text style={stylesEmployeeList.employeeName}>{item.nome}</Text>
-                  <Text style={stylesEmployeeList.employeeInfo}>CPF: {item.cpf}</Text>
-                </View>
-              </View>
-              <View style={stylesEmployeeList.buttonsContainer}>
-                <TouchableOpacity
-                  onPress={() => navigate('employerDashboard', { employeeId: item.id, companyId })}
-                >
-                  <DotsThree size={35} weight="bold" color="#4F67D8" />
-                </TouchableOpacity>
+                <Text style={stylesEmployeeList.employeeName}>{item.nome}</Text>
+                <Text style={stylesEmployeeList.employeeInfo}>CPF: {item.cpf}</Text>
               </View>
             </View>
           )}
@@ -94,4 +66,3 @@ export default function DemisionList({ companyId }: Props) {
     </View>
   );
 }
-
