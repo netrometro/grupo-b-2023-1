b953817911de673ea22a57a2f81e2e77e5d042ab
elise <Elisenovaes10@gmail.com>
2023-08-29 17:39:18 -0300

    Update de rota
    Update de rota de EmployerDashboard

--- frontend/src/screens/EmployerDashboard/index.tsx
+++ frontend/src/screens/EmployerDashboard/index.tsx
@@ -130,7 +130,7 @@ export default function EmployerDashboard({ route }: EmployerDashboardProps) {
             icon={<ClockClockwise size={38} weight="bold" color="#4F67D8" />}
           />
           <IconButton
-            onPress={() => navigate('FaltaDashboard', {employeeId: employeeId})}
+            onPress={() => navigate('faltaDashboard', { employeeId: employeeId })}
             text="Faltas"
             icon={<XCircle size={38} weight="bold" color="#4F67D8" />}
           />
