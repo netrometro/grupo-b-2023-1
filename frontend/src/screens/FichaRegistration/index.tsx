c616c1f4a2c685d6181e79b7a1174a623e3438c4
elise <Elisenovaes10@gmail.com>
2023-09-05 20:15:39 -0300

    Revert "Update index.tsx"
    This reverts commit 64f407ba8c5e05c16ae21d8760e6e2edbc708fe1.

--- frontend/src/screens/FichaRegistration/index.tsx
+++ frontend/src/screens/FichaRegistration/index.tsx
@@ -83,7 +83,6 @@ export default function FichaRegistration({ route }: FichaRegistrationProps) {
         };
   
         console.log('Id da empresa' + companyId);
-        console.log('cpf' + cpf);
   
         await api.post(`/createFicha/${companyId}`, reqData, { headers });
         ToastAndroid.show('Ficha Adicionada', ToastAndroid.LONG);
