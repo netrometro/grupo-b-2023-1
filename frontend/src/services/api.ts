0fff8fb289ad51b5a69be4436943f778ba312a5b
Nome <miranhaomi@gmail.com>
2023-09-05 18:58:12 -0300

    Update api.ts

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -3,3 +3,7 @@ import axios from 'axios';
 export const api = axios.create({
   baseURL: "https://optistaff-api.onrender.com",
 });
+
+export const viaCepApi = axios.create({
+  baseURL: "https://viacep.com.br/ws",
+});
