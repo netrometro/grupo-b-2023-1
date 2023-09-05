8fcf73f4c76b6ca42a9aaae863c3441d0eebc063
Nome <miranhaomi@gmail.com>
2023-09-05 20:57:38 -0300

    Update index.tsx

--- frontend/src/screens/CompanyRegistration/index.tsx
+++ frontend/src/screens/CompanyRegistration/index.tsx
@@ -51,7 +51,6 @@ export default function CompanyRegistration() {
       const { logradouro } = response.data;
       setCompanyAddress(logradouro);
     } catch (error) {
-      console.error(error);
     }
   }
 
