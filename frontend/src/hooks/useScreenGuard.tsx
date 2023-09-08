12d7b18a52b1961d17346d583c89bdc2188e23f5
Lucas Monteiro <lucasmonteiromelo@hotmail.com>
2023-09-08 11:48:03 -0300

    feat: finalizando a autenticação para entrar na tela de editar informações

--- frontend/src/hooks/useScreenGuard.tsx
+++ frontend/src/hooks/useScreenGuard.tsx
@@ -4,12 +4,22 @@ import { useEffect } from 'react';
 
 export function useScreenGuard(screenName: string) {
   const navigationState = useNavigationState((state) => state);
+
+  async function handleAuthentication() {
+    const auth = await LocalAuthentication.authenticateAsync({
+      promptMessage: 'Desbloqueie para acessar esta tela',
+    });
+
+    if (!auth.success) {
+      handleAuthentication();
+    }
+  }
   useEffect(() => {
     if (navigationState.routes) {
       const currentScreen = navigationState.routes[navigationState.index];
 
       if (currentScreen.name === screenName) {
-        console.log('Desbloquear tela');
+        handleAuthentication();
       }
     }
   }, []);
